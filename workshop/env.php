cf8885e786a7d52403c8178ee59d01ab0d7815cd
fbhood <mail@fabiopacifici.com>
2024-05-03 10:59:49 +0200

    refactor connection logic into DB class

---
+++ workshop/env.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_SERVERNAME', 'localhost:5506');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', '119_university');
--- workshop/index.php
+++ workshop/index.php
@@ -1,27 +1,20 @@
 <?php
 
-define('DB_SERVERNAME', 'localhost:5506');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', '119_university');
+require_once __DIR__ . '/helpers/functions.php';
 
-
-$mysqli = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if ($mysqli && $mysqli->connect_error) {
-  echo 'Database mysqli Failed';
-  die;
-}
+require_once __DIR__ . '/helpers/DB.php';
 
 
 
-var_dump($mysqli);
+$mysqli = DB::connect();
 
 $sql = "SELECT * FROM `courses`";
 $result = $mysqli->query($sql);
 
-/* var_dump($result);
-die; */
+dd($mysqli, $result);
+
+DB::disconnect($mysqli);
+
 
 ?>
 
